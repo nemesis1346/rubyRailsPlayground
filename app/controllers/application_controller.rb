d7d378dd2b678e0fdc6abcdd5958cc24d9f712ad
root <marco.maigua@nobleprog.eu>
2019-12-09 10:32:51 -0500

    trying to fix the org_people variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protect_from_forgery with: :exception
+
+  # To permit new custom attributes to be verified as attributes permitted by the form
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+  end
 end
