5b52869fc9d33024616ac6c19a9f6cd52353a1ee
root <marco.maigua@nobleprog.eu>
2019-12-25 14:22:18 -0500

    rename folder

---
+++ courseHackTutorial/app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(_resource)
+    has_contact_info = OrgContact.find_by(org_person_id: current_org_person.id)
+    if !has_contact_info.nil?
+      if !session[:return_to].nil?
+        session[:return_to]
+        session.delete[:return_to]
+      else
+        edit_org_person_path(current_org_person.id)
+      end
+    else
+      edit_org_person_path(current_org_person.id)
+    end
+  end
+
+  # To permit new custom attributes to be verified as attributes permitted by the form
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  end
+
+  
+end
