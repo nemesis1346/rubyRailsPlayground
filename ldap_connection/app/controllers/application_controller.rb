3adbd3bc7180633dadff9f23694c31f7ab3d1456
root <marco.maigua@nobleprog.eu>
2019-12-25 23:12:53 -0500

    ruby trainning ldap

---
+++ ldap_connection/app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!
+
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
+
+end
